71b575efe04389caf3845134094c5af3f2115b9f
caro-turnbull <caro.turnbull@gmail.com>
2023-05-31 14:04:48 -0400

    the middle function is good

--- middle.js
+++ middle.js
@@ -1,63 +1,58 @@
-
-
-const eqArrays = function(aghrr1, arr2){
-  let result
-  for (let i in arr1){
-    if (arr1[i] !== arr2[i]){
-    result = false
-  } else {
-  result = true }
-  }   
-  return result
-}
-
-
-
-
-const assertEqualArrays = function(eqArrays, expected) {
-  for (let i in arr1){
-    if (arr1[i] !== arr2[i]){
-    actual = false
-  } else {
-  actual = true }
-  }   
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed: ✅ ${actual} === ${expected}`)
   } else if (actual !== expected) {
     console.log(`Assertion Failed: ❌ ${actual} !== ${expected}`)
   }
+};
+
+const eqArrays = function(actual, expected){
+  let equalResult
+  for (let i of actual){
+    if (actual[i] !== expected[i]){
+    equalResult = false
+  } else {
+  equalResult = true }
+  }   
+  return equalResult
 }
 
 
-assertEqualArrays([1, 2, 3], [1, 2, 3], true); // => should PASS
-assertEqualArrays([1, 2, 3], [3, 2, 1], false); // => false
-
-assertEqualArrays(["1", "2", "3"], ["1", "2", "3"], true); // => true
-assertEqualArrays(["1", "2", "3"], ["1", "2", 3], false); // => false
-*/
 ///////
+
+const assertEqualArrays = function(actual, expected) { //actual is the result of middle func and expected is given in the test 
+  assertEqual(eqArrays(actual, expected), true)   // the eqArrays function returns true or false, so I should always compare to true to know if it worked???
+}
+
+////////
 
 const middle = function(array){
 
-  let result = []
+  let actual = []
   let middleIndex =0
 
   //if array is 2 or less, return empty array
    if (array.length <=2){
-    return result
+    return actual
   }
    //see if array length is even number (then return 2 values)
    else if (array.length %2 === 0) {
     middleIndex = (array.length / 2)
-    result.push (array[middleIndex], array[middleIndex+1])
-    console.log(result)
+    actual.push (array[middleIndex], array[middleIndex+1])
+    return actual
    }
    //if array is odd number in length, return 1 values
    else if (array.length %2 === 1) {
     middleIndex = (Math.ceil(array.length / 2))
-    result.push(array[middleIndex])
-    return result
+    actual.push(array[middleIndex])
+    return actual
    }
    }
 
-console.log (middle([1, 2]))
+
+   //test cases pass an array through the middle function
+   // and then that result along with the  expected result through assertEqualArray(which contacts eqArray)
+assertEqualArrays(middle([1, 2, 3, 4]), [2, 3]);  
+assertEqualArrays(middle([2, 3, 4, 5, 6, 7, 8]), [5]);
+assertEqualArrays(middle([1]), []);
+assertEqualArrays(middle(["a", "b", "c"]), ["b"]);
