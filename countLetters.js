6a52e1522e5d574093617227d401fce0b55354eb
caro-turnbull <caro.turnbull@gmail.com>
2023-05-31 17:38:03 -0400

    counted

---
+++ countLetters.js
@@ -0,0 +1,15 @@
+let countLetters = function(string){
+  let results = {}
+  for (let letter of string){
+    if (letter !== " "){
+      if (results[letter]){ // an if statement that just check is something exists
+      results[letter] += 1 //if it exists add one
+      } else{
+        results[letter] = 1 // if its the first time, start it at one
+      }
+    }
+  }
+  return results
+}
+
+console.log(countLetters("Lighthouse labs Ha"))
