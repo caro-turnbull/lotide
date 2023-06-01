138285ecb121efcda3818182592c99f10a9333e3
caro-turnbull <caro.turnbull@gmail.com>
2023-05-31 20:25:07 -0400

    linted

--- letterPositions.js
+++ letterPositions.js
@@ -1,15 +1,15 @@
 const letterPositions = function(sentence) {
   const results = {};
-  for (let i = 0; i< sentence.length; i++){
+  for (let i = 0; i < sentence.length; i++) {
     let letter = sentence[i];
-    if (letter !== " "){  //check that its not a space
-      if (!results[letter]){ //if there is not already a property/key for this letter
-        results[letter] = [] //if not, create an empty array
+    if (letter !== " ") {  //check that its not a space
+      if (!results[letter]) { //if there is not already a property/key for this letter
+        results[letter] = []; //if not, create an empty array
       }
-      results[letter].push(i) //  push a index into an array
-    } 
+      results[letter].push(i); //  push a index into an array
+    }
   }
   return results;
-}
+};
 
-console.log(letterPositions("lighthouse in the house"))
+console.log(letterPositions("lighthouse in the house"));
