1a78faada3298b7c4dc23ca5c09bbe2af8c9f4c6
caro-turnbull <caro.turnbull@gmail.com>
2023-06-05 20:08:02 -0400

    not all functions in the index have been seperated from their tests

---
+++ index.js
@@ -0,0 +1,21 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays')
+const countLetters = require('./countLetters')
+const eqObjects = require('./eqObjects')
+const countOnly = require('./countOnly')
+const letterPositions = require('./letterPositions')
+const findKeyByValue = require('./findKeyByValue')
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  eqArrays,
+  eqObjects,
+  countLetters, 
+  countOnly,
+  letterPositions,
+  findKeyByValue,
+};
